6be1ed33d09a23f1d65695e903a9da7382bc4bb6
alu0100951844 <alu0100951844@ull.edu.es>
2021-11-16 19:13:43 +0000

    Entrando en despliegue

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Aprendiendo a usar un generador",
+        "excerpt":"Por fin es jueves   Y hoy por lo menos el cañón ha funcionado …   \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\] ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/aet",
+        "teaser": null
+      },{
+        "title": "Mi segundo post",
+        "excerpt":"Este es mi segundo post First name Last name Age Location John Doe 35 United States Jane Doe 29 France Jack Hill 25 Australia Me gusta comer naranjas Me gusta comer peras Me gusta comer limones Me gusta comer fresas Somos 30 alumnos Twitter Mi usuario es Yeray-twitter Hoy es...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet",
+        "teaser": null
+      },{
+        "title": "Clase",
+        "excerpt":"Clase del jueves 11/11/2021   Añadiendo otra clase …  ","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/clase",
+        "teaser": null
+      }]
